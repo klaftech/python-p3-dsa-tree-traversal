fce13e51e76ba5bc28332e6d2462abd7b458e9b2
Elchonon <ekn400@gmail.com>
2024-12-30 12:30:26 -0500

    lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,17 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_look = [self.root]
+    
+    while len(nodes_to_look) > 0:
+      node = nodes_to_look.pop(0)
+      print('NODE HERE:')
+      print(node)
+      node_id = node['id']
+      if node_id and node_id == id:
+        print("SUCCESS")
+        return node
+      else:
+        nodes_to_look = nodes_to_look + node['children']
+    
+    return None
